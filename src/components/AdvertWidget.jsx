e4ab5d34ae7b47d6c8d48713740f92c0d295891d
superstahn <superstahn@gmail.com>
2024-01-19 17:13:03 +0100

    made some ui changes to form for post upload

--- src/components/AdvertWidget.jsx
+++ src/components/AdvertWidget.jsx
@@ -4,24 +4,30 @@ function AdvertWidget() {
   const imageAddress =
     "https://cdn.pixabay.com/photo/2015/08/25/03/51/lemon-906141_1280.jpg";
   return (
-    <WidgetWrapper>
-      <div className="flex items-center justify-between py-2 ">
-        <span className="text-sm text-black/40 dark:text-white">Sponsored</span>
-        <span className="text-sm text-black/40 dark:text-white">Create Ad</span>
-      </div>
-      <img src={imageAddress} alt="ad" />
-      <div className="flex items-center justify-between py-2">
-        <span className="text-sm">Nature's glow</span>
-        <span className="text-sm font-bold text-black/60 dark:text-white">
-          glow@gmail.com
-        </span>
-      </div>
-      <p className="text-sm text-black/60 dark:text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-        praesentium qui quaerat! Quaerat sit perspiciatis officia perferendis
-        tenetur optio cupiditate earum molestias sint?
-      </p>
-    </WidgetWrapper>
+    <div className="mt-5 md:mt-0">
+      <WidgetWrapper>
+        <div className="flex items-center justify-between py-2 ">
+          <span className="text-sm text-black/40 dark:text-white">
+            Sponsored
+          </span>
+          <span className="text-sm text-black/40 dark:text-white">
+            Create Ad
+          </span>
+        </div>
+        <img src={imageAddress} alt="ad" />
+        <div className="flex items-center justify-between py-2">
+          <span className="text-sm">Nature's glow</span>
+          <span className="text-sm font-bold text-black/60 dark:text-white">
+            glow@gmail.com
+          </span>
+        </div>
+        <p className="text-sm text-black/60 dark:text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
+          praesentium qui quaerat! Quaerat sit perspiciatis officia perferendis
+          tenetur optio cupiditate earum molestias sint?
+        </p>
+      </WidgetWrapper>
+    </div>
   );
 }
 
--- src/components/MyPostWidget.jsx
+++ src/components/MyPostWidget.jsx
@@ -58,62 +58,64 @@ function MyPostWidget() {
     }
   }, [image]);
   return (
-    <WidgetWrapper>
-      <div>
-        <div className="flex space-x-1 items-center border-b border-black/40 py-2 mb-2">
-          <UserImage image={imageAddress} />
-          <div className="flex-1 ">
-            <input
-              type="text"
-              className="w-full py-4 px-1 rounded-xl outline-none dark:bg-slate-900"
-              placeholder="What's on your mind?"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="flex items-center justify-between">
-          <label htmlFor="image">
-            <div className="flex items-center space-x-2">
-              <FaImage />
-              <span className="text-sm">Add Image</span>
-            </div>
-          </label>
-          <input
-            type="file"
-            id="image"
-            className="hidden sr-only"
-            // onChange={(e) => setImage(e.target.files[0])}
-            onChange={(e) => setImage(e.target.files[0])}
-          />
-          <button
-            onClick={createPost}
-            disabled={loading}
-            className="bg-blue-500 dark:bg-orange-500 dark:text-white rounded-xl w-[6rem] py-1 text-white"
-          >
-            {loading ? "Loading..." : " Post"}
-          </button>
-        </div>
-        {selectedImageUrl && (
-          <div className="mt-4 border border-dashed dark:border-white border-gray-700 text-3xl ">
-            <div className="relative w-[100px] h-[150px] my-2">
-              <img
-                src={selectedImageUrl}
-                alt=""
-                className="w-[100px] h-[150px] object-cover"
+    <div className="mt-5 md:mt-0">
+      <WidgetWrapper>
+        <div className="">
+          <div className="flex space-x-1 items-center border-b border-black/40 py-2 mb-2">
+            <UserImage image={imageAddress} />
+            <div className="flex-1 ">
+              <input
+                type="text"
+                className="w-full py-4 px-1 rounded-xl outline-none dark:bg-slate-900"
+                placeholder="What's on your mind?"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
-              <button
-                className="absolute top-0 right-0 z-10"
-                onClick={removeImage}
-              >
-                <IoIosCloseCircle size={20} className="text-white" />
-              </button>
-              <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
             </div>
           </div>
-        )}
-      </div>
-    </WidgetWrapper>
+          <div className="flex items-center justify-between">
+            <label htmlFor="image">
+              <div className="flex items-center space-x-2">
+                <FaImage />
+                <span className="text-sm">Add Image</span>
+              </div>
+            </label>
+            <input
+              type="file"
+              id="image"
+              className="hidden sr-only"
+              // onChange={(e) => setImage(e.target.files[0])}
+              onChange={(e) => setImage(e.target.files[0])}
+            />
+            <button
+              onClick={createPost}
+              disabled={loading}
+              className="bg-blue-500 dark:bg-orange-500 dark:text-white rounded-xl w-[6rem] py-1 text-white"
+            >
+              {loading ? "Loading..." : " Post"}
+            </button>
+          </div>
+          {selectedImageUrl && (
+            <div className="mt-4 border border-dashed dark:border-white border-gray-700 text-3xl ">
+              <div className="relative w-[100px] h-[150px] my-2">
+                <img
+                  src={selectedImageUrl}
+                  alt=""
+                  className="w-[100px] h-[150px] object-cover"
+                />
+                <button
+                  className="absolute top-0 right-0 z-10"
+                  onClick={removeImage}
+                >
+                  <IoIosCloseCircle size={20} className="text-white" />
+                </button>
+                <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
+              </div>
+            </div>
+          )}
+        </div>
+      </WidgetWrapper>
+    </div>
   );
 }
 
