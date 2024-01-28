74aff29f374c8cee973904f1434db54b2b259aa1
superstahn <superstahn@gmail.com>
2024-01-28 22:28:12 +0100

    changed baseUrl of axios

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:3001/api/v1",
+// });
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://social-app-api-be78.onrender.com/api/v1",
 });
 
 export default axiosInstance;
